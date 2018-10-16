98546432de1626b67efeb2f11e91c9f17dbbcfa7
Michael Wild <michael.wild@andritz.com>
2018-10-16 09:14:17 +0200

    Fix for python3 map not having count()

--- powerline_shell/segments/jobs.py
+++ powerline_shell/segments/jobs.py
@@ -13,7 +13,12 @@ class Segment(ThreadedSegment):
             output_proc = subprocess.Popen(['ps', '-af'], stdout=subprocess.PIPE)
             output = map(lambda l: int(l.split()[2].strip()),
                 output_proc.communicate()[0].decode("utf-8").splitlines()[1:])
-            self.num_jobs = output.count(os.getppid()) - 1
+            cnt = -1
+            ppid = os.getppid()
+            for pid in output:
+                if pid == ppid:
+                    cnt += 1
+            self.num_jobs = cnt
         else:
             pppid_proc = subprocess.Popen(['ps', '-p', str(os.getppid()), '-oppid='],
                                           stdout=subprocess.PIPE)
